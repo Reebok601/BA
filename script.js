f031b59ffc617ea10d2e1c394d1a992074af8f14
Ribak Dc <ribakdc@Ribaks-MacBook-Pro.local>
2022-07-18 00:38:15 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = 10;
+document.querySelector('.score').textContent = 13;
